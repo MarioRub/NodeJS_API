9d374f191b5fe1f017f0f96016231b1ac7c64dd6
Mario Rubio <mario.dagoberto.rubio@gmail.com>
2020-07-28 13:53:57 -0600

    Fix Start Script

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/tasks",{useNewUrlParser:true})
+mongoose.connect("mongodb://localhost/tasks",{useNewUrlParser:true, useUnifiedTopology:true })
 .then(db=>console.log("Connection Sucessful"))
 .catch(err=>console.error);
 
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require ("express");
 const app = express();
 const { mongoose } = require("./db")
 const cors =require("cors");
+const port = process.env.PORT || 3000;
+
 
 app.use(express.json());
 app.use(cors({origin: 'http://localhost:3000'}));
@@ -9,6 +11,6 @@ app.use(cors({origin: 'http://localhost:3000'}));
 
 app.use("/", require("./server/route/routes.js"));
 
-app.listen(3001,(req,res)=>{
-    console.log("Servidor Listo");
+app.listen(port,(req,res)=>{
+    console.log("Server Running");
 });
