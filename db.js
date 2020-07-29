aa41679d76fa3c503eb57efc87c74da582e9c380
Mario Rubio <mario.dagoberto.rubio@gmail.com>
2020-07-29 09:03:54 -0600

    MongoDB String Connection

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://mariorub:<password>@clustermariorub.l05ig.mongodb.net/taskapp?retryWrites=true&w=majority";
+const uri = "mongodb+srv://mariorub:Mar1122.@clustermariorub.l05ig.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
 
 mongoose.connect(uri,{useNewUrlParser:true, useUnifiedTopology:true })
 .then(db=>console.log("Connection Sucessful"))
--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require ("express");
 const app = express();
 const { mongoose } = require("./db")
-const cors =require("cors");
 const port = process.env.PORT || 3000;
 
-
 app.use(express.json());
-app.use(cors({origin: 'http://localhost:3000'}));
-
 
 app.use("/", require("./server/route/routes.js"));
 
