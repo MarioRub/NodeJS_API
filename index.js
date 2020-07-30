c192ad810c5c9e934552d61e9c224c15454192ae
Mario Rubio <mario.dagoberto.rubio@gmail.com>
2020-07-30 09:32:58 -0600

    Add cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require ("express");
 const app = express();
-const { mongoose } = require("./db")
+const { mongoose } = require("./db");
+const cors =require("cors");
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.use(cors({origin: 'http://localhost:3000'}));
 app.use("/", require("./server/route/routes.js"));
 
 app.listen(port,(req,res)=>{
