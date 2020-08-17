d540c4a0fed017a8b4ba81e1206c63bc63e308e8
Mario Rubio <mario.dagoberto.rubio@gmail.com>
2020-08-17 17:07:55 -0600

    Adding cors fix

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require ("express");
 const app = express();
 const { mongoose } = require("./db");
+const cors =require("cors");
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.options('*', cors())
+
 app.use("/", require("./server/route/routes.js"));
 
 app.listen(port,(req,res)=>{
