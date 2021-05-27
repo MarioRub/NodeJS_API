3f5a9ab87d7e4d55314941730e56cdb253cf3096
Mario Rubio <mario.dagoberto.rubio@gmail.com>
2021-05-27 10:39:10 -0600

    Changing Port

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const { mongoose } = require("./db");
-const cors =require("cors");
-const port = process.env.PORT || 3000;
+const cors = require("cors");
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.options('*', cors())
 
 app.use("/", require("./server/route/routes.js"));
 
-app.listen(port,(req,res)=>{
+app.listen(port, (req, res) => {
     console.log("Server Running");
 });
